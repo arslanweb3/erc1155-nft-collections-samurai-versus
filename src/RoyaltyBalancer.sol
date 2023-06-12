f486bae5e2d23ef7bcdda32e87c1159dc7f584dd
Arslan Galimov <111274950+arslngalimov@users.noreply.github.com>
2023-06-12 16:09:32 +0500

    Update RoyaltyBalancer.sol

--- src/RoyaltyBalancer.sol
+++ src/RoyaltyBalancer.sol
@@ -1,16 +1,27 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity 0.8.17;
 
 import {IRoyaltyBalancer} from "./IRoyaltyBalancer.sol";
 import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
+import {ReentrancyGuard} from "openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
 
-contract RoyaltyBalancer is IRoyaltyBalancer, Ownable {
+contract RoyaltyBalancer is IRoyaltyBalancer, Ownable, ReentrancyGuard { 
 
   /* ****** */
   /* ERRORS */
   /* ****** */
 
-  error OnlyCollection();
+  error OnlyCollection(string message);
+  error NoShares();
+  error EmptyContractBalance();
+
+  /* ****** */
+  /* EVENTS */
+  /* ****** */
+
+  event ClaimedReward(address claimer, uint256 claimedRewardAmount);
+  event ReceivedRoyaltyFunds(address sender, uint256 receivedAmount);
+  event AddedMinterShares(address minter, uint256 shares);
 
   /* ******* */
   /* STORAGE */
@@ -25,11 +36,11 @@ contract RoyaltyBalancer is IRoyaltyBalancer, Ownable {
   }
 
   uint256 public totalShares;
-  uint256 public accRewardPerShare;
+  uint256 public accumulatedRewardPerShare;
 
   modifier onlyCollection() {
     if (msg.sender != collection) {
-      revert OnlyCollection();
+      revert OnlyCollection("Only collection of Genesis Water/Fire Samurai can add minter shares to this contract");
     }
     _;
   }
@@ -56,30 +67,40 @@ contract RoyaltyBalancer is IRoyaltyBalancer, Ownable {
   function addMinterShare(address minter, uint256 amount) external onlyCollection {
     totalShares = totalShares + amount;
     userInfo[minter].shares += amount;
-    userInfo[minter].debt = accRewardPerShare * userInfo[minter].shares;
+    userInfo[minter].debt = accumulatedRewardPerShare * userInfo[minter].shares;
+    emit AddedMinterShares(minter, amount);
   }
 
   // @notice Allows to see how many rewards minter has (in wei)
   function pendingReward(address minter) public view returns (uint256) {
     UserInfo storage user = userInfo[minter];
-    return accRewardPerShare * user.shares - user.debt;
+    return accumulatedRewardPerShare * user.shares - user.debt;
   }
 
   // @notice Allows any user that has 'shares' to claim his rewards 
-  function claimReward() external /* can be executed by any user */ {
+  function claimReward() external nonReentrant /* can be executed by any user */ {
     UserInfo storage user = userInfo[msg.sender];
     if (user.shares == 0) {
-      return;
+      revert NoShares();
     }
+
     uint256 pending = pendingReward(msg.sender);
-    user.debt = accRewardPerShare * user.shares;
+    if (pending == 0) {
+      revert EmptyContractBalance();
+    }
+
+    user.debt = accumulatedRewardPerShare * user.shares;
+
     (bool success, ) = msg.sender.call{value: pending}("");
     require(success, "Couldn't send minter claiming funds");
+
+    emit ClaimedReward(msg.sender, pending);
   }
 
   // @notice When someone (supposed to be marketplace) sends funds to this contract (which is set as royalty receiver), 
-  // reward per 1 share for each minter increases
+  // accumulated reward per 1 share for each minter increases
   receive() external payable {
-    accRewardPerShare = accRewardPerShare + msg.value / totalShares;
+    accumulatedRewardPerShare = accumulatedRewardPerShare + msg.value / totalShares;
+    emit ReceivedRoyaltyFunds(msg.sender, msg.value);
   }
 }
